7b4985d2bda1fbe294ff1faff2a9c180065f5d46
edivrapa <blu_terranova@hotmail.com>
2016-01-15 13:05:25 -0600

    00-Saludo

---
+++ 00-saludo/src/Saludo.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author Edgar Iván Ramírez Pavía
+ *
+ */
+public class Saludo {
+
+	/**
+	 * 
+	 * @param Mandamos a imprimir desde consola un saludo 
+	 */
+	public static void main(String[] args) {
+		//JOptionPane.showMessageDialog(null, "Hola Edgar");
+		System.out.println("Hola, desde consola");
+	}
+
+}
